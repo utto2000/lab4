07ece4d1e85f56b3f54d5cc94f9bd7bef6d8c1ec
utto2000 <bartek200039@gmail.com>
2022-01-15 18:10:01 +0100

    Add files via upload

---
+++ MvcGames/Data/MvcGamesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcGames.Data
+{
+    public class MvcGamesContext : DbContext
+    {
+        public MvcGamesContext (DbContextOptions<MvcGamesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Games> Games { get; set; }
+    }
+}
